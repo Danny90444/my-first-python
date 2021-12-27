3250c08863087dc14c8a912a3dda27e3dc9cc84e
Daniel Lear <dlear.mcsc@gmail.com>
2021-12-26 21:22:53 -0500

    practice

--- Hello.py
+++ Hello.py
@@ -48,3 +48,26 @@ print(fractional_part(5, 2)) # Should be 0.5
 print(fractional_part(5, 0)) # Should be 0
 print(fractional_part(0, 5)) # Should be 0
 
+def format_name(first_name, last_name):
+	if last_name and first_name:
+		return ("Name: " + last_name + ", " + first_name)
+	if len(first_name) > 0: 
+		return("Name: " + first_name)
+	elif len(last_name) > 0: 
+		return("Name: " + last_name )
+	elif not first_name and last_name:
+		return ("")
+
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
+
