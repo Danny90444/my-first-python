fad81b7cbbd8bf67993fbcd077fb8d863ad85778
Daniel Lear <dlear.mcsc@gmail.com>
2022-05-11 17:56:08 -0400

    nesting while loop inside function

--- while-and-for-loops.py
+++ while-and-for-loops.py
@@ -1,5 +1,8 @@
-x = 0
-while x < 10:
- print("X is only " + str(x))
- x+=1
- print("X is now = " + str(x)) 
+def test_function():
+    x = 0
+    while x < 10:
+     print("X is only " + str(x))
+     x+=1
+    print("X is now = " + str(x)) 
+
+test_function()
