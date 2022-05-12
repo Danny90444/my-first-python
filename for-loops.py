a22361064e2cb3b52a6ddbc7f620f4c53ccc1309
Daniel Lear <dlear.mcsc@gmail.com>
2022-05-12 15:24:10 -0400

    nested for loop

--- for-loops.py
+++ for-loops.py
@@ -8,3 +8,8 @@ print(factorial(4)) # should return 24
 print(factorial(5)) # should return 120
 
 
+teams = ['Dragons' , 'Wolves' , 'Pandas' , 'Unicorns']
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team + "vs" + away_team)
