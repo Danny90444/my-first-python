72a566b1181765345a10dae16878941a15da253e
Daniel Lear <dlear.mcsc@gmail.com>
2022-05-12 14:55:46 -0400

    working now

--- for-loops.py
+++ for-loops.py
@@ -1,8 +1,8 @@
 def factorial(n):
     result = 1
-    for x in range(1,n):
-     print(x)
-     
+    for x in range(1,n+1):
+        result = result * x
+    return result
 
 print(factorial(4)) # should return 24
 print(factorial(5)) # should return 120
