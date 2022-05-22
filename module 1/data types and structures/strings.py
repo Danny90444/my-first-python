ec57e6a9effd6178b89637a2d2bcc6f25ec83d68
Daniel Lear <dlear.mcsc@gmail.com>
2022-05-22 19:40:41 -0400

    fix

--- module 1/data types and structures/strings.py
+++ module 1/data types and structures/strings.py
@@ -6,3 +6,15 @@ def double_word(word):
 print(double_word("hello")) # Should return hellohello10
 print(double_word("abc"))   # Should return abcabc6
 print(double_word(""))      # Should return 0
+
+def first_and_last(message):
+    if len(message) == 0:
+     return False
+    elif message[0] == message[-1]:
+      return True
+    else:
+      return False
+
+print(first_and_last("else"))
+print(first_and_last("tree"))
+print(first_and_last(""))
