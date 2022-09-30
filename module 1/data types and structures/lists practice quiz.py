d0eb91e425c4b0ab099da9e12190945953f6fac0
Daniel Lear <dlear.mcsc@gmail.com>
2022-09-29 21:18:42 -0400

    Finishing list quest

--- module 1/data types and structures/lists practice quiz.py
+++ module 1/data types and structures/lists practice quiz.py
@@ -1,3 +1,6 @@
+from mimetypes import guess_type
+
+
 def pig_latin(text):
   say = ""
   # Separate the text into words
@@ -27,3 +30,51 @@ print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay
 #Pat is 35 years old and works as Lawyer
 #Amanda is 25 years old and works as Engineer
 #"""
+
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+
+newfilenames = []
+for file in filenames:
+    if  ".hpp" in file:
+     newfilenames.append(file[:-2])
+    else: newfilenames.append(file)
+print(newfilenames)
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4,"r"),(2,"w"),(1,"x")]
+    # Iterate over each of the digits in octal
+    for element in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if element >= value:
+                result += letter
+                element -= value
+            else:
+                result += "-"
+    return result
+    
+print(octal_to_string(755)) # Should be rwxr-xr-x
+print(octal_to_string(644)) # Should be rw-r--r--
+print(octal_to_string(750)) # Should be rwxr-x---
+print(octal_to_string(600)) # Should be rw-------
+
+def guest_list(guests):
+	for index,guest in enumerate(guests):
+		name,age,job = guest
+		print("{} is {} years old and works as {}".format(name, age, job))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+#Click Run to submit code
+"""
+Output should match:
+Ken is 30 years old and works as Chef
+Pat is 35 years old and works as Lawyer
+Amanda is 25 years old and works as Engineer
+"""
+
+
