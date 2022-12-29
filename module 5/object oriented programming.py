c7a45c00ae1bed8df487f21f0ebac29cafdf8d61
Daniel Lear <dlear.mcsc@gmail.com>
2022-12-28 19:43:09 -0500

    Starting a new chapter

--- module 5/object oriented programming.py
+++ module 5/object oriented programming.py
@@ -192,3 +192,11 @@ class Turtle(Animal):
     category = 'reptile'
 
 print(Turtle.category)
+
+class Snake(Animal):
+    catergory = 'reptile'
+
+def sum(x, y):
+		return(x+y)
+print(sum(sum(1,2), sum(3,4)))
+print (((10 >= 5*2) and (10 <= 5*2)))
